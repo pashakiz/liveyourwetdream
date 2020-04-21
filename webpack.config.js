55ea6369889ed755d9ab546147f43927bac5ecb1
pashakiz <pashakiz@gmail.com>
2020-04-21 16:24:27 +0300

    setting webpack 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,74 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserJSPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
+  mode: 'development', // "production" | "development" | "none"
   entry: [
     './src/js/index.js',
     './src/scss/style.scss'
   ],
   output: {
-    filename: './js/bundle.js'
+    filename: './js/bundle.[contenthash].js',
+    path: path.resolve(__dirname, 'dist')
   },
   devtool: "source-map",
   module: {
-    rules: [{
-        test: /\.js$/,
-        include: path.resolve(__dirname, 'src/js'),
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: 'env'
+    rules: [
+        {
+          test: /\.js$/,
+          include: path.resolve(__dirname, 'src/js'),
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: 'env'
+            }
           }
-        }
-      },
-      {
-        test: /\.(sass|scss)$/,
-        include: path.resolve(__dirname, 'src/scss'),
-        use: MiniCssExtractPlugin.extract({
-          use: [{
-              loader: "css-loader",
-              options: {
-                sourceMap: true//,
-                //url: false
+        },
+        {
+          test: /\.(sass|scss)$/,
+          include: path.resolve(__dirname, 'src/scss'),
+          use: MiniCssExtractPlugin.extract({
+            use: [{
+                loader: "css-loader",
+                options: {
+                  sourceMap: true//,
+                  //url: false
+                }
+              },
+              {
+                loader: "resolve-url-loader"
+              },
+              {
+                loader: "sass-loader",
+                options: {
+                  sourceMap: true
+                }
               }
-            },
+            ]
+          })
+        },
+        {
+          test: /\.(png|jpg|svg|gif)$/,
+          use: [
             {
-              loader: "resolve-url-loader"
-            },
-            {
-              loader: "sass-loader",
-              options: {
-                sourceMap: true
-              }
+              loader: 'file-loader',
+              options: {name: 'img/[name].[ext]'}
             }
           ]
-        })
-      },
-      {
-        test: /\.(png|jpg|gif)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {name: 'img/[name].[ext]'}
-          }
-        ]
-      },
+        },
+        {
+          test: /\.(ttf|otf|svg|woff|woff2|eot)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {name: 'fonts/[name].[ext]'}
+            }
+          ]
+        },
     ]
   },
   optimization: {
@@ -72,6 +85,13 @@ module.exports = {
     },
   },
   plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/html/views/index.html'
+    }),
+    // new HtmlWebpackPlugin({  // Also generate a test.html
+    //   filename: 'test.html',
+    //   template: './src/html/views/test.html'
+    // }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
       filename: './css/style.bundle.css',
