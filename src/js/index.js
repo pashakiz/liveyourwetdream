9077738247f636a585132c0ed4572353e09cc6c9
pashakiz <pashakiz@gmail.com>
2020-04-27 22:53:08 +0300

    privacy.html done, anytext.html done, fix sticky footer, mobile menu,

--- src/js/index.js
+++ src/js/index.js
@@ -6,5 +6,32 @@ import '@scss/main.scss'
 import '../js/babel'
 
 $(function() {
-    //$('body').css('color', 'blue');
+
+    //preloader
+    setTimeout(function() {
+        var preloader = $('#page-preloader');
+        if ( !preloader.hasClass('done') ) {
+            preloader.addClass('done');
+        }
+    }, 1000);
+
+    //mobile menu expand
+    $('.header').on('click', '.header__nav-toggle', function(){
+        let header_menu = $(this).closest('.header').find('.header-menu');
+        if ( $('.header').hasClass('expand') ) {
+            header_menu.slideUp(300);
+            setTimeout(function() {
+                $('.header').removeClass('expand');
+            }, 300);
+        } else {
+            header_menu.slideDown(300);
+            $('.header').addClass('expand');
+        }
+    });
+
+    //upload photo
+    $('.custom-file-input').on('change', function(){
+        $(this).closest('.custom-file').addClass('image');
+    });
+
 });
